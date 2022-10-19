06d873d95286af66d0c5c842b1e5f7229f3d1bb2
gaoqiangks <gaoqiangks@qq.com>
2022-10-20 05:39:37 +0800

    aa

--- 极小曲面/mathematica/sequence_of_catenoid.m
+++ 极小曲面/mathematica/sequence_of_catenoid.m
@@ -1,2 +1,8 @@
 Show[Map[ContourPlot[Cosh[#*y] == #*x, {x, -2, 2}, {y, -2, 2}, 
     Axes -> True] &, Table[Log[x], {x, 10, 10000, 1000}]]]
+
+
+a = Map[ContourPlot[Cosh[#*y] == #*x, {x, -2, 2}, {y, -2, 2}, Axes -> True] &, List[1, 2, 3, 5, 10, 100]]	
+b = ComplexRegionPlot[3/8*Pi < Arg[z] < 5/8*Pi, {z, -2 - 2 I, 2 + 2 I}]	
+c = ComplexRegionPlot[-5/8*Pi < Arg[z] < -3/8*Pi, {z, -2 - 2 I, 2 + 2 I}]
+Show[a,b,c]
